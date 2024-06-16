cd014cdc2215b4e5444e599fe06d2c51778bd9a6
dakshsawhneyy <darkstardaksh@gmail.com>
2024-06-16 23:24:55 +0530

    Final

--- script.js
+++ script.js
@@ -32,11 +32,30 @@ const showSlider = (type) => {
     }, 2000)
 }
 seeMoreButtons.forEach((button) => {
-    button.onclick = function(){
+    button.addEventListener("click", () => {
         carousel.classList.remove('next', 'prev');
         carousel.classList.add('showDetail');
-    }
-});
+    })
+})
 backButton.onclick = function(){
     carousel.classList.remove('showDetail');
 }
+
+
+// document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+// function ctrlShiftKey(e, keyCode) {
+//     return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+// }
+
+// document.onkeydown = (e) => {
+//   // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+//     if (
+//     event.keyCode === 123 ||
+//     ctrlShiftKey(e, 'I') ||
+//     ctrlShiftKey(e, 'J') ||
+//     ctrlShiftKey(e, 'C') ||
+//     (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+//     )
+//     return false;
+// };
